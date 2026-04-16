e4e38dfca3c379305786fb18475f00339946c669
Archer <Fenrisulfr.33@protonmail.com>
2026-04-16 14:22:23 -0600

    Moves list fixed to account for empty array

--- frontend/app/pokemon/abilities/page.jsx
+++ frontend/app/pokemon/abilities/page.jsx
@@ -2,8 +2,8 @@ import AbilitiesList from "./components/AbilitiesList";
 
 const getAbilities = async () => {
   const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/pokemon/abilities`);
-  const responseData = await response.json();
-  return Array.isArray(responseData) ? responseData : [];
+  const abilities = await response.json();
+  return Array.isArray(abilities) ? abilities : [];
 }
 
 export default async function AbilitiesPage() {
--- frontend/app/pokemon/moves/page.jsx
+++ frontend/app/pokemon/moves/page.jsx
@@ -1,9 +1,13 @@
 // import MovesList from "@/components/pokemon/MovesList";
 import MovesList from "./components/MovesList";
 
-export default async function Moves() {
+const getMoves = async () => {
   const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/pokemon/moves`);
   const moves = await response.json();
+  return Array.isArray(moves) ? moves : [];
+}
 
+export default async function Moves() {
+  const moves = await getMoves();
   return <MovesList list={moves} />;
 }
